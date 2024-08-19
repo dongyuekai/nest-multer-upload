565fcf26d21ed5ebcae09b5dbe8d32e052f45c02
dongyuekai <457545648@qq.com>
2024-08-19 14:54:56 +0800

    feat: 指定字段文件上传

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,7 +8,11 @@ import {
   UploadedFiles,
 } from '@nestjs/common';
 import { AppService } from './app.service';
-import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import {
+  FileFieldsInterceptor,
+  FileInterceptor,
+  FilesInterceptor,
+} from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
@@ -41,6 +45,28 @@ export class AppController {
   )
   uploadFiles(
     @UploadedFiles() files: Array<Express.Multer.File>,
+    @Body() body,
+  ) {
+    console.log('body---', body);
+    console.log('files---', files);
+  }
+
+  // 多个文件字段
+  @Post('ccc')
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'aaa', maxCount: 2 },
+        { name: 'bbb', maxCount: 3 },
+      ],
+      {
+        dest: 'myUploads',
+      },
+    ),
+  )
+  uploadFileFields(
+    @UploadedFiles()
+    files: { aaa?: Express.Multer.File[]; bbb?: Express.Multer.File[] },
     @Body() body,
   ) {
     console.log('body---', body);
