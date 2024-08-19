2cf3ff091454ac36d8d837ab133d1a6952f1d7fa
dongyuekai <457545648@qq.com>
2024-08-19 14:23:02 +0800

    feat: 单文件上传

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+  Body,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
@@ -8,5 +16,17 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('aaa')
+  @UseInterceptors(
+    // 使用FileInterceptor来提取aaa字段 然后通过UploadedFile装饰器把它作为参数传入
+    FileInterceptor('aaa', {
+      dest: 'uploads',
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File, @Body() body) {
+    console.log('body---', body);
+    console.log('file---', file);
   }
 }
